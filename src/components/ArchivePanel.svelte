ce6803acb4cc1a722091162ba8d8f9695268f133
FUCLU <24521382@gm.uit.edu.vn>
2026-07-29 02:20:47 +0700

    update2

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -137,8 +137,8 @@ onMount(async () => {
                             {#each post.data.tags as tag}
                                 <a
                                     href={getTagUrl(tag)}
-                                    aria-label={`View all posts with the ${tag.trim()} tag`}
-                                    class="max-w-full text-xs font-semibold text-50 transition hover:text-[var(--primary)]"
+                                    aria-label={View all posts with the ${tag.trim()} tag}
+                                    class="max-w-full rounded-md bg-black/[0.03] px-2 py-0.5 text-xs font-semibold text-50 transition hover:border-[var(--primary)] hover:bg-[var(--primary)]/10 hover:text-[var(--primary)] dark:bg-white/[0.06] dark:hover:border-[var(--primary)] dark:hover:bg-[var(--primary)]/15"
                                 >
                                     <span class="block overflow-hidden text-ellipsis whitespace-nowrap">#{tag.trim()}</span>
                                 </a>
